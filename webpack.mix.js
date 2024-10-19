9a6e388268ce54e0e431d0c8f36962e434f747de
Ivan <gia691992men@yandex.ru>
2024-10-19 20:12:35 +0300

    Добавил ссылки

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/react/src/index.js', 'public/js')  // Путь к React-коду
+mix.js('resources/js/react/src/index.js', 'public/js')    // Путь к React-коду
     .react()                                              // Обработка React
     .setPublicPath('public')                              // Путь к сборке
     .sourceMaps();                                        // Источники для отладки
